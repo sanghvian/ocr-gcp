8238457742ea19d37bead40c8c46356644943718
ankit <69717009+crew-guy@users.noreply.github.com>
2021-09-01 08:55:50 +0530

    redux

--- src/redux/reducers/patientReducer.ts
+++ src/redux/reducers/patientReducer.ts
@@ -1,8 +1,17 @@
-
+import * as actions from '@actions/actionTypes'
+import { defaultCipherList } from 'constants'
 const initialState = {}
 
 const patientReducer = (state:any, action:any) => {
-    
+    switch (action.type) {
+        case actions.ADDING_REPORT:
+            console.log('report is being uploaded')
+            return {
+                ...state
+            }
+        default:
+            console.log('default case triggered')
+    }
 }
 
 export default patientReducer
